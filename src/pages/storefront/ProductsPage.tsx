fb86f77e35c440085f8326ef13d3b4a47758e382
[dyad] <aberzak1997@gmail.com>
2026-05-20 04:51:56 +0100

    [dyad] Increasing product grid to 3 columns - wrote 1 file(s)

--- src/pages/storefront/ProductsPage.tsx
+++ src/pages/storefront/ProductsPage.tsx
@@ -156,7 +156,7 @@ const ProductsPage = () => {
                 </Button>
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-[5px]">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[5px]">
                 {filteredProducts.map(product => (
                   <ProductCard key={product.id} product={product} />
                 ))}
