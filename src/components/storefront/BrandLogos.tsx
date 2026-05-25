aa0301e1f3c0e0531ebd88c771879cbfe99e0991
[dyad] <aberzak1997@gmail.com>
2026-05-25 05:47:46 +0100

    [dyad] Fixed the Samsung logo by using a wordmark-only version - wrote 1 file(s)

--- src/components/storefront/BrandLogos.tsx
+++ src/components/storefront/BrandLogos.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const BrandLogos = () => {
   const brands = [
     { name: "Apple", logo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" },
-    { name: "Samsung", logo: "https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg" },
+    { name: "Samsung", logo: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Samsung_wordmark.svg" },
     { name: "Sony", logo: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Sony_logo.svg" },
     { name: "Microsoft", logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg" },
     { name: "Google", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" },
